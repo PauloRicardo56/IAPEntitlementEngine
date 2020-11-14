b4ead96b9a4bb6fdcdec1bb39257bf1e2225325b
Paulo Ricardo <praraujo.vieira@gmail.com>
2020-11-14 12:48:27 -0300

    create: subscription migration config v1

--- Sources/App/Migrations/CreateTodo.swift
+++ Sources/App/Migrations/CreateTodo.swift
@@ -1,14 +1,17 @@
 import Fluent
 
-struct CreateTodo: Migration {
+struct CreateSubscription: Migration {
     func prepare(on database: Database) -> EventLoopFuture<Void> {
-        return database.schema("todos")
+        return database.schema("subscriptions")
             .id()
-            .field("title", .string, .required)
+            .field("original_transaction_id", .string, .required)
+            .field("subscription_status", .string, .required)
+            .field("purchase_date", .string, .required)
+            .field("product_id", .string, .required)
             .create()
     }
 
     func revert(on database: Database) -> EventLoopFuture<Void> {
-        return database.schema("todos").delete()
+        return database.schema("subscriptions").delete()
     }
 }
