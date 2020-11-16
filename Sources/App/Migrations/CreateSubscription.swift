20dbc23e226f92f12f7f53a2700b849f5c8629d8
Paulo Ricardo <praraujo.vieira@gmail.com>
2020-11-16 16:18:53 -0300

    create: last json fields added

--- Sources/App/Migrations/CreateSubscription.swift
+++ Sources/App/Migrations/CreateSubscription.swift
@@ -4,16 +4,19 @@ struct CreateSubscription: Migration {
     func prepare(on database: Database) -> EventLoopFuture<Void> {
         return database.schema("subscriptions")
             .id()
-            .field("notification_type", .string, .required)
-            .field("environment", .string, .required)
+            .field("auto_renew_adam_id", .string)
             .field("auto_renew_product_id", .string, .required)
-            .field("auto_renew_status", .string, .required)
+            .field("auto_renew_status", .string)
+            .field("auto_renew_status_change_date", .string)
+            .field("auto_renew_status_change_date_ms", .string)
+            .field("auto_renew_status_change_date_pst", .string)
+            .field("environment", .string, .required)
+            .field("expiration_intent", .int)
+            .field("notification_type", .string, .required)
+            .field("password", .string)
+            .field("unified_receipt", .string, .required)
             .field("bid", .string, .required)
             .field("bvrs", .string, .required)
-            .field("auto_renew_status_change_date", .string, .required)
-            .field("auto_renew_status_change_date_ms", .string, .required)
-            .field("auto_renew_status_change_date_pst", .string, .required)
-            .field("unified_receipt", .string, .required)
             .create()
     }
 
@@ -21,12 +24,3 @@ struct CreateSubscription: Migration {
         return database.schema("subscriptions").delete()
     }
 }
-
-//@Field(key: "auto_renew_status_change_date")
-//var auto_renew_status_change_date: String
-//
-//@Field(key: "auto_renew_status_change_date_ms")
-//var auto_renew_status_change_date_ms: String
-//
-//@Field(key: "auto_renew_status_change_date_pst")
-//var auto_renew_status_change_date_pst: String
--- Sources/App/Models/Subscription.swift
+++ Sources/App/Models/Subscription.swift
@@ -6,18 +6,39 @@ final class Subscription: Model, Content {
     
     @ID(key: .id)
     var id: UUID?
-
-    @Field(key: "notification_type")
-    var notification_type: String
     
-    @Field(key: "environment")
-    var environment: String
+    @Field(key: "auto_renew_adam_id")
+    var auto_renew_adam_id: String?
     
     @Field(key: "auto_renew_product_id")
     var auto_renew_product_id: String
     
     @Field(key: "auto_renew_status")
-    var auto_renew_status: String
+    var auto_renew_status: String?
+    
+    @Field(key: "auto_renew_status_change_date")
+    var auto_renew_status_change_date: String?
+    
+    @Field(key: "auto_renew_status_change_date_ms")
+    var auto_renew_status_change_date_ms: String?
+    
+    @Field(key: "auto_renew_status_change_date_pst")
+    var auto_renew_status_change_date_pst: String?
+    
+    @Field(key: "environment")
+    var environment: String
+    
+    @Field(key: "expiration_intent")
+    var expiration_intent: Int?
+
+    @Field(key: "notification_type")
+    var notification_type: String
+    
+    @Field(key: "password")
+    var password: String?
+    
+    @Field(key: "unified_receipt")
+    var unified_receipt: UnifiedReceipt
     
     @Field(key: "bid")
     var bid: String
@@ -25,103 +46,37 @@ final class Subscription: Model, Content {
     @Field(key: "bvrs")
     var bvrs: String
     
-    @Field(key: "auto_renew_status_change_date")
-    var auto_renew_status_change_date: String
-    
-    @Field(key: "auto_renew_status_change_date_ms")
-    var auto_renew_status_change_date_ms: String
-    
-    @Field(key: "auto_renew_status_change_date_pst")
-    var auto_renew_status_change_date_pst: String
-    
-    @Field(key: "unified_receipt")
-    var unified_receipt: UnifiedReceipt
-    
     init() { }
 
     init(
         id: UUID? = nil,
-        notification_type: String,
-        environment: String,
+        auto_renew_adam_id: String?,
         auto_renew_product_id: String,
-        auto_renew_status: String,
+        auto_renew_status: String?,
+        auto_renew_status_change_date: String?,
+        auto_renew_status_change_date_ms: String?,
+        auto_renew_status_change_date_pst: String?,
+        environment: String,
+        expiration_intent: Int?,
+        notification_type: String,
+        password: String?
+        unified_receipt: UnifiedReceipt,
         bid: String,
         bvrs: String,
-        auto_renew_status_change_date: String,
-        auto_renew_status_change_date_ms: String,
-        auto_renew_status_change_date_pst: String,
-        unified_receipt: UnifiedReceipt
     ) {
         self.id = id
-        self.notification_type = notification_type
-        self.environment = environment
+        self.auto_renew_adam_id = auto_renew_adam_id
         self.auto_renew_product_id = auto_renew_product_id
         self.auto_renew_status = auto_renew_status
-        self.bid = bid
-        self.bvrs = bvrs
         self.auto_renew_status_change_date = auto_renew_status_change_date
         self.auto_renew_status_change_date_ms = auto_renew_status_change_date_ms
         self.auto_renew_status_change_date_pst = auto_renew_status_change_date_pst
+        self.environment = environment
+        self.expiration_intent = expiration_intent
+        self.notification_type = notification_type
+        self.password = password
         self.unified_receipt = unified_receipt
+        self.bid = bid
+        self.bvrs = bvrs
     }
 }
-
-
-//"notification_type": "DID_RENEW"
-//"environment": "Sandbox"
-//    "auto_renew_product_id": "com.QuintetoFantastico.MacroChallenge.sharedbusiness",
-//    "auto_renew_status": "true",
-//    "bid": "com.QuintetoFantastico.MacroChallenge",
-//    "bvrs": "1",
-//    "unified_receipt": {
-//        "status": 0,
-//        "environment": "Sandbox",
-//        "latest_receipt_info": [
-//            {
-//                "quantity": "1",
-//                "product_id":  "com.QuintetoFantastico.MacroChallenge.sharedbusiness",
-//                "transaction_id": "1000000742037931",
-//                "purchase_date": "2020-11-15 03:31:03 Etc/GMT",
-//                "purchase_date_ms": "1605411063000",
-//                "purchase_date_pst": "2020-11-14 19:31:03 America/Los_Angeles",
-//                "original_purchase_date": "2020-11-15 03:26:05 Etc/GMT",
-//                "original_purchase_date_ms": "1605410765000",
-//                "original_purchase_date_pst": "2020-11-14 19:26:05 America/Los_Angeles",
-//                "expires_date": "2020-11-15 03: 36: 03 Etc/GMT",
-//                "expires_date_ms": "1605411363000",
-//                "expires_date_pst": "2020-11-14 19:36:03 America/Los_Angeles",
-//                "web_order_line_item_id": "1000000057404390",
-//                "is_trial_period": "false",
-//                "is_in_intro_offer_period": "false",
-//                "original_transaction_id": "1000000742037455",
-//                "subscription_group_identifier": "20708206"
-//            },
-//            {
-//                "quantity": "1",
-//                "product_id": "com.QuintetoFantastico.MacroChallenge.sharedbusiness",
-//                "transaction_id": "1000000742037455",
-//                "purchase_date": "2020-11-15 03:26:03 Etc/GMT",
-//                "purchase_date_ms": "1605410763000",
-//                "purchase_date_pst": "2020-11-14 19:26:03 America/Los_Angeles",
-//                "original_purchase_date": "2020-11-15 03:26:05 Etc/GMT",
-//                "original_purchase_date_ms": "1605410765000",
-//                "original_purchase_date_pst": "2020-11-14 19:26:05 America/Los_Angeles",
-//                "expires_date": "2020-11-15 03:31:03 Etc/GMT",
-//                "expires_date_ms": "1605411063000",
-//                "expires_date_pst": "2020-11-14 19:31:03 America/Los_Angeles",
-//                "web_order_line_item_id": "1000000057404389",
-//                "is_trial_period": "false",
-//                "is_in_intro_offer_period": "false",
-//                "original_transaction_id": "1000000742037455",
-//                "subscription_group_identifier": "20708206"
-//            }
-//        ],
-//        "pending_renewal_info": [
-//            {
-//                "auto_renew_status": "1",
-//                "auto_renew_product_id": "com.QuintetoFantastico.MacroChallenge.sharedbusiness",
-//                "product_id": "com.QuintetoFantastico.MacroChallenge.sharedbusiness",
-//                "original_transaction_id": "1000000742037455"
-//            }
-//        ],
-//        "latest_receipt":
